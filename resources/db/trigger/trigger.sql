e5ce7ae65d1553ca05103e3cd5d58a55f36aaf67
Nhan Tran <rim25102004@gmail.com>
2024-04-22 21:43:08 +0300

    Change Comment for each trigger

--- resources/db/trigger/trigger.sql
+++ resources/db/trigger/trigger.sql
@@ -9,13 +9,13 @@ BEGIN
     DECLARE doctor_role VARCHAR(255);
     DECLARE patient_role VARCHAR(255);
 
-    -- Kiểm tra doctor_id
+    -- Check doctor_id
     SELECT role INTO doctor_role FROM users WHERE id = NEW.doctor_id;
     IF doctor_role != 'doctor' THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot insert appointment for non-doctor user';
     END IF;
 
-    -- Kiểm tra patient_id
+    -- Check patient_id
     SELECT role INTO patient_role FROM users WHERE id = NEW.patient_id;
     IF patient_role != 'patient' THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot insert appointment for non-patient user';
@@ -34,7 +34,7 @@ BEGIN
     DECLARE user_role VARCHAR(255);
 
     SELECT role INTO user_role FROM users WHERE id = NEW.doctor_id;
-
+    -- Check doctor role for user
     IF user_role != 'doctor' THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot insert specialization for non-doctor user';
     END IF;
@@ -55,17 +55,17 @@ BEGIN
 
     IF OLD.status_id != NEW.status_id THEN
         IF OLD.status_id IS NULL THEN
-            -- Trạng thái cũ là NULL, tức là cuộc hẹn mới được tạo
-            -- Gửi thông báo cho bác sĩ
+            -- If old status is NULL, new appointment is created
+            -- Send notification to doctor
             SET recipient_id = NEW.doctor_id;
             SET recipient_role = 'doctor';
             SET message = CONCAT('Patient ',
                                  (SELECT CONCAT(first_name, ' ', last_name) FROM users WHERE id = NEW.patient_id),
                                  ' has just created an appointment with you.');
         ELSE
-            -- Trạng thái cũ không phải là NULL, tức là cuộc hẹn đã tồn tại và có thay đổi trạng thái
+            -- If old status is not NULL, current appointment exists and send notification to patient
             IF NEW.doctor_id IS NOT NULL THEN
-                -- Bác sĩ đã thay đổi trạng thái của cuộc hẹn, gửi thông báo cho bệnh nhân
+                -- Doctor change status then send notification to patient
                 SET recipient_id = NEW.patient_id;
                 SET recipient_role = 'patient';
                 SET message = CONCAT('Appointment status is changed to ',
@@ -75,7 +75,7 @@ BEGIN
             END IF;
         END IF;
 
-        -- Thêm thông báo vào bảng notifications
+        -- Add new notification into table notifications
         INSERT INTO notifications (appointment_id, recipient_id, recipient_role, message, created_at, status_change)
         VALUES (NEW.id, recipient_id, recipient_role, message, NOW(), TRUE);
     END IF;
